ff84d562da1e7f78f2da251ebb6d13f201a33815
nuel232 <nwankwoala3@gmail.com>
2025-08-28 15:16:43 +0100

    feat: integrate dotenv for API key management, add team model, and enhance home page with team data fetching

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,102 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:nba_app/models/team.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Team> teams = [];
 
   //get teams
   Future getTeams() async {
-    var response = await http.get(Uri.https('api.balldontlie.io', '/v1/teams'));
-    print(response.body);
+    var response = await http.get(
+      Uri.https('api.balldontlie.io', '/v1/teams'),
+      headers: {'Authorization': dotenv.env['API_KEY'] ?? ''},
+    );
+    var jsonData = jsonDecode(response.body);
+
+    for (var eachTeam in jsonData['data']) {
+      final team = Team(
+        abbreviation: eachTeam['abbreviation'],
+        city: eachTeam['city'],
+      );
+      teams.add(team);
+    }
+
+    print(teams.length);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTeams(); // Actually call the function here!
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('NBA TEAMS', style: TextStyle(fontWeight: FontWeight.bold)),
+        title: Text('NBA TEAMS'),
         centerTitle: true,
-        backgroundColor: Colors.blue[900],
+        backgroundColor: Colors.blue[500],
+      ),
+
+      //when you make a request to an API, it takes time to get the response that why you use a Future builder
+      body: FutureBuilder(
+        future: getTeams(),
+        builder: (context, snapshot) {
+          //is it done loading? then show team data
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: teams.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[200],
+
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  padding: EdgeInsets.all(10),
+
+                  child: ListTile(
+                    title: Text(teams[index].abbreviation),
+                    subtitle: Text(teams[index].city),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.blue[600],
+                      child: Text(
+                        teams[index].abbreviation[0],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                  ),
+                );
+              },
+            );
+          }
+          //if it is still loading show loading circle
+          else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
